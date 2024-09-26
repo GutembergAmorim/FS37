d28de3f99f45ee880273a5ed84dfaaf959e4fe7e
GutembergAmorim <gutemberg.py@gmail.com>
2024-09-26 20:31:49 -0300

    BuscaCep

--- Aula_24_React_Router_Dom/projeto/src/pages/Buscar/BuscarCep.jsx
+++ Aula_24_React_Router_Dom/projeto/src/pages/Buscar/BuscarCep.jsx
@@ -1,35 +1,56 @@
 import { useState } from "react";
-import { json } from "react-router-dom";
-
 
 
 const BuscarCep = () => {
 
-    const [cep, setCep] = useState('');
-    const [endereco, setEndereco] = useState('');
+    const [cep, setCep] = useState('');    
+    const [rua, setRua] = useState('');
+    const [bairro, setBairro] = useState('');
+    const [cidade, setCidade] = useState('');
 
     async function ColetarCep(){
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const data = await response.json();
         
-        setEndereco(`${data.logradouro}, ${data.bairro}, ${data.localidade}, - ${data.uf}`)
+        
+        setRua(`${data.logradouro}`)
+        setBairro(`${data.bairro}`)
+        setCidade(`${data.localidade}`)
     }
 
     return ( 
         <>
             <div className="container">
                 <div className="row">
+                    <h2>Buscar Endereco por CEP</h2>
                     <div className="col-md-4 offset-md-4 mt-5">
                         <div className="mb-2">
                             <label htmlFor="">Digite o CEP</label>
                             <input type="text" name="cep" id="cep" className="form-control" onChange={(e) => { setCep(e.target.value)}} />
                         </div>
                         <div className="mb-2">
-                            <button className="btn btn-primary btn-sm" onClick={ColetarCep}>Buscar</button>
+                            <label htmlFor="">Rua</label>
+                            <input type="text" name="rua" id="rua" className="form-control" disabled value={rua}/>
                         </div>
-                        <span>
-                            <div id="cep" className="container" dangerouslySetInnerHTML={{__html: endereco}}></div>
-                        </span>
+                        <div className="mb-2">
+                            <label htmlFor="">Numero</label>
+                            <input type="text" name="numero" id="numero" className="form-control" />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="">Complemento</label>
+                            <input type="text" name="complemento" id="complemento" className="form-control" />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="">Bairro</label>
+                            <input type="text" name="bairro" id="bairro" className="form-control" disabled value={bairro} />
+                        </div>
+                        <div className="mb-2">
+                            <label htmlFor="">Cidade</label>
+                            <input type="text" name="cidade" id="cidade" className="form-control" disabled value={cidade} />
+                        </div>
+                        <div className="mb-2">
+                            <button className="btn btn-primary btn-sm" onClick={ColetarCep}>Buscar</button>
+                        </div>                        
                     </div>
                 </div>
             </div>
